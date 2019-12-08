bc4b2c89f00a721551eeca2226d8bd89dda8f0f0
Khurram_AMU <41499600+khurram6968@users.noreply.github.com>
2019-12-08 18:57:05 +0530

    Add files via upload

---
+++ Influence observation in data.R
@@ -0,0 +1,23 @@
+mydata = read.csv("C:\\Users\\MKhurram\\Desktop\\stats.csv")
+model = lm(mydata$Scoring.Avg.~mydata$Greens.in.Reg.+mydata$Putting.Avg.)
+par(mfrow=c(0,0))
+plot(model, which=c(1,1))
+library(olsrr)
+ols_plot_resid_stud_fit(model)
+library(ggplot2)
+# find influence observa
+df <- model$df.residual
+p <- length(model$coefficients)
+n <- nrow(model$model)
+dffits_crit = 2 * sqrt((p + 1) / (n - p - 1))
+influence3_dffits <- dffits(model)
+df <- data.frame(obs = names(influence3_dffits),
+                 dffits = influence3_dffits)
+ggplot(df, aes(y = dffits, x = obs)) +
+  geom_point() +
+  geom_hline(yintercept = c(dffits_crit, -dffits_crit), linetype="dashed",) +
+  labs(title = "DFFITS",
+       subtitle = "Influential Observation",
+       x = "Observation Number",
+       y = "DFFITS")+theme(axis.text.x = element_text(angle = 60))
+
